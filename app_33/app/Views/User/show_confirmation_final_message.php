3c137311691d271fb4860de08594fd28b2e299f6
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-03 09:20:24 -0300

    Update

--- app_33/app/Views/User/show_confirmation_final_message.php
+++ app_33/app/Views/User/show_confirmation_final_message.php
@@ -7,39 +7,10 @@
         <div class="col-sm-10 col-lg-6 col-xl-5">
 
             <div class="card p-5">
-                <h3 class="text-center">Login</h3>
+                <h3 class="text-center">Nova conta de usuário</h3>
                 <hr>
 
-                <?= form_open('login_submit', ['novalidate' => true]) ?>
-
-                <div class="mb-3">
-                    <label for="text_username" class="form-label">Email</label>
-                    <input type="email" name="text_username" id="text-username" class="form-control" placeholder="Digite seu Email" required value="<?= old('text_username') ?>">
-                    <?php if(!empty($validation_errors)): ?>
-                        <span class="text-danger fst-italic"><small><?= show_form_errors('text_username', $validation_errors) ?></small></span>
-                    <?php endif; ?>  
-                </div>
-
-                <div class="mb-3">
-                    <label for="text_passwrd" class="form-label">Senha</label>
-                    <input type="password" name="text_passwrd" id="text_passwrd" class="form-control" placeholder="Digite sua senha" required>
-                    <?php if(!empty($validation_errors)): ?>
-                        <span class="text-danger fst-italic"><small><?= show_form_errors('text_passwrd', $validation_errors) ?></small></span>
-                    <?php endif; ?>
-                </div>
-
-                <div class="mb-3 text-center">
-                    <a href="#">Esqueci a minha senha</a>
-                    <span class="mx-2 opacity-25">|</span>
-                    <a href="<?= site_url('new_user_account_frm')?>">Criar nova conta</a>
-                </div>
-
-                <div class="mb-3 text-center">
-                    <input type="submit" value="Login" class="btn btn-primary w-50">
-                </div>
-
-                <?= form_close() ?>  
-
+                <p class="text-center">Verifique a sua caixa de correio. <br>Enviamos um email para conclusão do registro</p>
             </div>
         </div>
     </div>
@@ -47,6 +18,7 @@
 
 
 <?= $this->endSection() ?>
+
 
 
 
