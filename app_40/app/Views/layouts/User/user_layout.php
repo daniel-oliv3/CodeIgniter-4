c09c5b8c8c6f42c0e9ca7d5e4d9d5152bc77c696
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-24 10:27:25 -0300

    Update

--- app_40/app/Views/layouts/User/user_layout.php
+++ app_40/app/Views/layouts/User/user_layout.php
@@ -8,6 +8,8 @@
 	<title><?= APP_NAME ?></title>
 	<!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/fontawesome/css/all.min.css') ?>">
 </head>
 <body class="bg-dark">
 
